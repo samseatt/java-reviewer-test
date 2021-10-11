f47f9dacb66ca2cb3d7f211e56e197217d040b23
Sam Seatt <samseatt@gmail.com>
2021-10-11 15:56:18 -0600

    Lab4 DynamoDBScan demo code fixed

--- DynamoDBScan.java
+++ DynamoDBScan.java
@@ -19,6 +19,8 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 public class DynamoDBScan implements RequestHandler<Map<String,String>, String> {
 
     static String tableName = "<FMI1>";
+    private static AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().withRegion("us-east-1").build();
+
 
     @Override
     public String handleRequest(Map<String,String> event, Context context)
@@ -27,7 +29,7 @@ public class DynamoDBScan implements RequestHandler<Map<String,String>, String> 
     }
 
     private static String findAllItems() {
-        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+//        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
         DynamoDB dynamoDB = new DynamoDB(client);
         Table table = dynamoDB.getTable(tableName);
 
